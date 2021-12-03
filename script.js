44b3485432ca2a50575f22da48b6f68e5225b991
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-12-03 09:34:26 +0100

    add wave effect

--- script.js
+++ script.js
@@ -2,18 +2,40 @@ $(document).ready(() => start())
 
 $('button').html('Text');
 
-function start() {
-    let text = $('.text').html();
+function generateRandomColor() {
+    let r = Math.floor(Math.random() * 256),
+        g = Math.floor(Math.random() * 256),
+        b = Math.floor(Math.random() * 256)
+    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+}
+
+function stylizeText1() {
+    const text = $('.text1').html();
     let newText = "";
 
     for (let i = 0; i < text.length; i++) {
-
         let random = Math.floor(1 + Math.random() * 5);
-
         if (text[i] == " ") newText += " ";
-        else newText += '<span class="effect' + random + '">' + text[i] + '</span>';
+        else newText += '<span style="color: ' + generateRandomColor() + '" class="effect' + random + '">' + text[i] + '</span>';
     }
 
-    $('.text').html(newText);
+    $('.text1').html(newText);
+}
+
+function stylizeText2() {
+    const text = $('.text2').html();
+    let newText = "";
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == " ") newText += " ";
+        else newText += '<span style="--i: ' + (i * 100 + 'ms') + '">' + text[i] + '</span>';
+    }
+
+    $('.text2').html(newText);
+}
+
+function start() {
+    stylizeText1();
+    stylizeText2();
 }
 
